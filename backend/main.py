46996cd6a002942cf761f1343e1c13e752de2123
Yan Ji <rglsm7655558@gmail.com>
2022-12-17 11:19:47 +0800

    reduce inverter communication

--- backend/main.py
+++ backend/main.py
@@ -2,20 +2,22 @@ import json
 import os
 from datetime import datetime
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
-
 from .db_operations import init_db, init_sql_files, insert_watt
 from .fronius_connector import FroniusConnector
 from .PGConnector import PGConnector
 
+
 app = FastAPI()
-
-
 app.add_middleware(
-    CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
@@ -26,23 +28,35 @@ class Item(BaseModel):
     intz: str
 
 
-@app.get("/api/save_watt")
-async def save_watt(request: Request):
+async def update_watt() -> None:
+    watt = await get_watt_from_inverter()
+    app.state.realtime_watt = {
+        "ts": datetime.now(),
+        "watt": watt,
+    }
+
+
+async def get_watt_from_inverter() -> None:
     try:
         watt = app.state.fc.fetch_realtime_watt()
-    except Exception as e:
+    except Exception as _:
         watt = 0
+    return watt
+
+
+@app.get("/api/save_watt")
+async def save_watt(request: Request) -> dict[str, None]:
+    watt = await get_watt_from_inverter()
     await insert_watt(app.state.db, watt)
     return {"watt": watt}
 
 
 @app.get("/api/get_watt")
-async def get_watt(request: Request):
-    try:
-        watt = app.state.fc.fetch_realtime_watt(3)
-    except Exception as e:
-        watt = 0
-    return {"watt": watt}
+async def get_watt(request: Request, background_tasks: BackgroundTasks):
+    last_updated_seconds_ago = (datetime.now() - app.state.realtime_watt["ts"]).total_seconds()
+    if last_updated_seconds_ago > 3:
+        background_tasks.add_task(update_watt)
+    return {"watt": app.state.realtime_watt["watt"]}
 
 
 @app.get("/api/get_watt_history")
@@ -75,5 +89,10 @@ async def startup():
     await pgcon.connect()
     app.state.db = pgcon
     app.state.fc = FroniusConnector(os.getenv("INVERTER_HOST"))
+    watt = await get_watt_from_inverter()
+    app.state.realtime_watt = {
+        "ts": datetime.now(),
+        "watt": watt,
+    }
     await init_db(pgcon)
     await init_sql_files(pgcon)
