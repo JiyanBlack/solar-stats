63730fc04cfbf4e2c1421e41744690c13f446e2f
Yan Ji <rglsm7655558@gmail.com>
2023-04-01 15:06:57 +0800

    change app point to local

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,8 @@ import timezone from "dayjs/plugin/timezone";
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-// const serverUrl = "http://localhost:3000";
-const serverUrl = "https://solarstats.tplinkdns.com";
+const serverUrl = "http://localhost:3000";
+// const serverUrl = "https://solarstats.tplinkdns.com";
 
 function App() {
   const [wattData, setWattData] = React.useState([]);
